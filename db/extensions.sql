21e2daa0ae221a350aa5c02e29022ba53f5ede02
fred.lpo07 <frederic.cloitre@lpo.fr>
2019-09-22 02:51:48 +0200

    WIP add extensions

---
+++ db/extensions.sql
@@ -0,0 +1,7 @@
+create extension if not exists postgis;
+create extension if not exists postgis_topology; 
+create extension if not exists pgcrypto; 
+create extension if not exists pg_trgm; 
+create extension if not exists "uuid-ossp"; 
+create extension if not exists ogr_fdw; 
+create extension if not exists postgres_fdw; 
